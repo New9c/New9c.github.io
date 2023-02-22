175907efc6388a917dea8e73be226d0f5f7316e7
New9c <125933011+New9c@users.noreply.github.com>
2023-02-22 19:19:29 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function credits() {
+    var credit_amount = document.getElementById("credit amount").value;
+    alert("credit amount is "+ credit_amount)
+    var needed_credit_amount = document.getElementById("needed credit amount").value;
+    alert("needed credit amount is "+ needed_credit_amount)
+}
