55b11065ec2b71b4437f72120616e9459b6693bc
New9c <125933011+New9c@users.noreply.github.com>
2023-02-26 06:43:56 +0800

    initial commit

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
 function start_everything(){
     document.getElementById("disclaimer").innerHTML = ""
-    document.getElementById("the good shit").innerHTML += "<div class='content starting_questions' id='starting_questions'>"
+    document.getElementById("the good shit").innerHTML += "<div class='content starting_questions' id='starting_question'>"
     document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById("starting_questions").innerHTML += "<label for='starting question 1'>What do you wanna know?</label><br><br>"
-    document.getElementById("starting_questions").innerHTML += `<input class='brawler button' type='button' value="Know when I'll get my new brawler" id='brawler button' onclick='make_question(1)'><br>`
-    document.getElementById("starting_questions").innerHTML += `<input class='future_days button' type='button' value="Know the rewards I'll get in ? days" id='future_days button' onclick='make_question(2)'>`
-}
-
-function make_question(type) {
-    if(type == 1)
-        var things = ["New Brawler", "brawler"]
-    if(type == 2)
-        var things = ["Days of Rewards", "future_days"]
-    document.getElementById("the good shit").innerHTML = ""
-    document.getElementById("the good shit").innerHTML += "<div class='content text starting_questions' id='tracker'>"
-    document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById("tracker").innerHTML += `${things[0]} >`
-    document.getElementById("the good shit").innerHTML += `<div class="content starting_questions" id="${things[1]} question">`
-    document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById(`${things[1]} question`).innerHTML += "<label for='starting question 2'>Choose an option (Fast = less questions & less accurate)</label><br><br>"
-    document.getElementById(`${things[1]} question`).innerHTML += `<input class="standard button" type="button" value="Standard Calculation" id="standard" onclick="make_${things[1]}_question(1)"><br>`
-    document.getElementById(`${things[1]} question`).innerHTML += `<input class="fast button" type="button" value="Fast Calculation" id="fast" onclick="make_${things[1]}_question(2)"><br>`
+    document.getElementById("starting_question").innerHTML += "<label for='starting question'>Choose a version (Fast = less questions & less accurate)</label><br><br>"
+    document.getElementById("starting_question").innerHTML += `<input class="standard button" type="button" value="Standard Version" id="standard" onclick="make_brawler_question(1)"><br>`
+    document.getElementById("starting_question").innerHTML += `<input class="fast button" type="button" value="Fast Version" id="fast" onclick="make_brawler_question(2)"><br>`
 }
 
 function make_brawler_question(type_sf){
@@ -31,10 +15,12 @@ function make_brawler_question(type_sf){
     document.getElementById("the good shit").innerHTML = ""
     document.getElementById("the good shit").innerHTML += "<div class='content text question' id='tracker'>"
     document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById("tracker").innerHTML += `New Brawler > ${things[0]} >`
+    document.getElementById("tracker").innerHTML += `${things[0]} > Rarity`
     document.getElementById("the good shit").innerHTML += "<div class='content rarity question' id='rarity question'>"
     document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById("rarity question").innerHTML += "<label for='starting question 2'>What rarity is your brawler?</label><br><br>"
+    document.getElementById("rarity question").innerHTML += "<label for='starting questions'>What's your brawler's name? (Optional, clicking on the rarity brings you to the next page)</label><br>"
+    document.getElementById("rarity question").innerHTML += "<input type='text' id='brawler name' oninput='brawler_name()'><br><br>"
+    document.getElementById("rarity question").innerHTML += "<label for='starting questions' id='wat rarity'>What rarity is your brawler?</label><br><br>"
     document.getElementById("rarity question").innerHTML += `<input class='rare button' type='button' value='Rare' id='rare' onclick='make_brawler_${things[1]}_questions(160)'> `
     document.getElementById("rarity question").innerHTML += `<input class='super_rare button' type='button' value='Super Rare' id='super rare' onclick='make_brawler_${things[1]}_questions(430)'> `
     document.getElementById("rarity question").innerHTML += `<input class='epic button' type='button' value='Epic' id='epic' onclick='make_brawler_${things[1]}_questions(925)'> `
@@ -44,11 +30,16 @@ function make_brawler_question(type_sf){
     document.getElementById("rarity question").innerHTML += `<input class="starting button" type="button" value="Starting Brawler" id="starting" onclick="starting('${things[0]}')">`
 }
 
+function brawler_name(){
+    the_brawler_name = document.getElementById("brawler name").value
+    document.getElementById("wat rarity").innerHTML = `What rarity is ${the_brawler_name}?`
+}
+
 function starting(text_4_tracker){
     document.getElementById("the good shit").innerHTML = ""
     document.getElementById("the good shit").innerHTML += "<div class='content text question' id='tracker'>"
     document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById("tracker").innerHTML += `New Brawler > ${text_4_tracker} > Starting Brawler`
+    document.getElementById("tracker").innerHTML += `${text_4_tracker} > Starting Brawler`
     document.getElementById("the good shit").innerHTML += "<div class='content result' id='result'>"
     document.getElementById("the good shit").innerHTML += "</div>"
     document.getElementById("result").innerHTML += "For new players:<br>"
@@ -59,14 +50,17 @@ function starting(text_4_tracker){
 }
 
 function make_brawler_standard_questions(credits_needed) {
+    the_rarity = make_the_rarity(credits_needed)
+    if (the_brawler_name == '')
+        the_brawler_name = "the brawler"
     document.getElementById("the good shit").innerHTML = ""
     document.getElementById("the good shit").innerHTML += "<div class='content text question' id='tracker'>"
     document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById("tracker").innerHTML += "New Brawler > Standard > Rarity >"
+    document.getElementById("tracker").innerHTML += `Standard > ${the_rarity} > Questions`
     document.getElementById("the good shit").innerHTML += "<div class='content question' id='brawler standard questions'>"
     document.getElementById("the good shit").innerHTML += "</div>"
     document.getElementById("brawler standard questions").innerHTML += "<input class='restart button disabled' type='button' value='Restart' id='restart button' onclick='restart()'><br><br>"
-    document.getElementById("brawler standard questions").innerHTML += "<label for='credit amount'>Credit amount (bottom left of the brawler's face):</label><br>"
+    document.getElementById("brawler standard questions").innerHTML += `<label for='credit amount'>Credit amount (bottom left of ${the_brawler_name}'s face):</label><br>`
     document.getElementById("brawler standard questions").innerHTML += "<input type='text' id='credit amount' oninput='restart_ready()'> credits<br><br>"
     needed_credit_amount = credits_needed;
     document.getElementById("brawler standard questions").innerHTML += "<label for='current token amount'>Current token amount (left bottom corner): </label><br>"
@@ -97,14 +91,17 @@ function put_slider_value(){
 }
 
 function make_brawler_fast_questions(credits_needed) {
+    the_rarity = make_the_rarity(credits_needed)
+    if (the_brawler_name == '')
+        the_brawler_name = "the brawler"
     document.getElementById("the good shit").innerHTML = ""
     document.getElementById("the good shit").innerHTML += "<div class='content text question' id='tracker'>"
     document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById("tracker").innerHTML += "New Brawler > Fast > Rarity >"
+    document.getElementById("tracker").innerHTML += `Fast > ${the_rarity} > Questions`
     document.getElementById("the good shit").innerHTML += "<div class='content question' id='brawler fast questions'>"
     document.getElementById("the good shit").innerHTML += "</div>"
     document.getElementById("brawler fast questions").innerHTML += "<input class='restart button disabled' type='button' value='Restart' id='restart button' onclick='restart()'><br><br>"
-    document.getElementById("brawler fast questions").innerHTML += "<label for='credit amount'>Credit amount(bottom left of the brawler's face):</label><br>"
+    document.getElementById("brawler fast questions").innerHTML += `<label for='credit amount'>Credit amount(bottom left of ${the_brawler_name}'s face):</label><br>`
     document.getElementById("brawler fast questions").innerHTML += "<input type='text' id='credit amount' oninput='restart_ready()'><br><br>"
     needed_credit_amount = credits_needed;
     document.getElementById("brawler fast questions").innerHTML += "<label for='have pass'>Do you have the brawl pass this season? </label><br>"
@@ -113,26 +110,19 @@ function make_brawler_fast_questions(credits_needed) {
     document.getElementById("brawler fast questions").innerHTML += "<input class='done button'  type='button' value='Done' id='done button' onclick='solve_brawler(2)'>"
 }
 
-function make_future_days_question(type_sf) {
-    if(type_sf == 1)
-        var things = ["Standard", "standard"]
-    if(type_sf == 2)
-        var things = ["Fast", "fast"]
-    document.getElementById("the good shit").innerHTML = ""
-    document.getElementById("the good shit").innerHTML += "<div class='content text question' id='tracker'>"
-    document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById("tracker").innerHTML += `Days of Rewards > ${things[0]} >`
-    document.getElementById("the good shit").innerHTML += `<div class="content question" id="future_days ${things[1]} question">`
-    document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById(`future_days ${things[1]} question`).innerHTML += "<input class='restart button disabled' type='button' value='Restart' id='restart button' onclick='restart()'><br><br>"
-    document.getElementById(`future_days ${things[1]} question`).innerHTML += "<label for='how many days?'>How many days of rewards do you want to know about?</label><br>"
-    document.getElementById(`future_days ${things[1]} question`).innerHTML += "<input type='text' id='days asked' oninput='restart_ready()'><br>"
-    document.getElementById(`future_days ${things[1]} question`).innerHTML += "<label for='have pass'>Do you have the brawl pass this season? </label><br>"
-    document.getElementById(`future_days ${things[1]} question`).innerHTML += "<input class='yes button' type='button' value='Yes' id='yes 4 pass' onclick='yes_4_pass()'> "
-    document.getElementById(`future_days ${things[1]} question`).innerHTML += "<input class='no button' type='button' value='No' id='no 4 pass' onclick='no_4_pass()'><br><br>"
-    document.getElementById(`future_days ${things[1]} question`).innerHTML += `<input class="done button"  type="button" value="Done" id="done button" onclick="solve_future_days(${type_sf})">`
+function make_the_rarity(input){
+    if(input==160)
+        var the_rarity = "Rare"
+    if(input==430)
+        var the_rarity = "Super Rare"
+    if(input==925)
+        var the_rarity = "Epic"
+    if(input==1900)
+        var the_rarity = "Mythic"
+    if(input==3800)
+        var the_rarity = "Legendary"
+    return the_rarity
 }
-
 function make_result_place() {
     document.getElementById("the good shit").innerHTML += "<div class='content result' id='the result'> "
     document.getElementById("the good shit").innerHTML += "</div>"
@@ -141,11 +131,20 @@ function make_result_place() {
 function make_end_place(ottf) {
     document.getElementById("the good shit").innerHTML += "<div class='content result' id='endnote'> "
     document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById("endnote").innerHTML += "Thanks for using my website! You can leave now, or reload the page to use it again, or find out more about the code's flaws.<br>"
-    if(ottf==1)
+    document.getElementById("endnote").innerHTML += "Thanks for using my website!<br>"
+    if(ottf==1){
+        document.getElementById("endnote").innerHTML += "Please note that this doesn't count a lot of ways you can make more progress, such as Club League, Challeges and Masteries.<br>Click on the button below for more info.<br>"
+        document.getElementById("endnote").innerHTML += "If you want to start over, simply reload the page.<br>"
+        document.getElementById("endnote").innerHTML += "Bye! :D<br>"
         document.getElementById("endnote").innerHTML += `<input class="assumptions button" type="button" value="Assumptions & The Unpredictable" id="assumptions" onclick="assumptions(1)"></input>`
-    else if (ottf==2)
+    }
+        
+    else if (ottf==2){
+        document.getElementById("endnote").innerHTML += "Please note that the fast version gives a very rough prediction, and its results should be taken with a MOUNTAIN of salt.<br>Click on the button below for more info.<br>"
+        document.getElementById("endnote").innerHTML += "If you want to start over, simply reload the page.<br>"
+        document.getElementById("endnote").innerHTML += "Bye! :D<br>"
         document.getElementById("endnote").innerHTML += `<input class="assumptions button" type="button" value="Assumptions & The Unpredictable" id="assumptions" onclick="assumptions(2)"></input>`
+    }
 }
 
 function assumptions(ottf) {
@@ -162,10 +161,12 @@ function assumptions(ottf) {
         document.getElementById("endnote").innerHTML += `* Believes you'll get the brawler next season at most<br>`
         document.getElementById("endnote").innerHTML += `<br>`
         document.getElementById("endnote").innerHTML += `The Unpredictable:<br>`
+        document.getElementById("endnote").innerHTML += `* Club League<br>`
+        document.getElementById("endnote").innerHTML += `* Masteries<br>`
         document.getElementById("endnote").innerHTML += `* JACKPOT!<br>`
         document.getElementById("endnote").innerHTML += `* Challenges / Surprise quests<br>`
         document.getElementById("endnote").innerHTML += `* Trophy road rewards<br>`
-        document.getElementById("endnote").innerHTML += `* Higher ranks/experience (additional tokens)<br>`
+        document.getElementById("endnote").innerHTML += `* Higher ranks (additional tokens)<br>`
         document.getElementById("endnote").innerHTML += `* Double token/Coin showers events<br>`
         document.getElementById("endnote").innerHTML += `* event.brawlstars.com`
     }
@@ -185,6 +186,8 @@ function assumptions(ottf) {
         document.getElementById("endnote").innerHTML += `* Believes you'll get the brawler next season at most<br>`
         document.getElementById("endnote").innerHTML += `<br>`
         document.getElementById("endnote").innerHTML += `The Unpredictable:<br>`
+        document.getElementById("endnote").innerHTML += `* Club League<br>`
+        document.getElementById("endnote").innerHTML += `* Masteries<br>`
         document.getElementById("endnote").innerHTML += `* JACKPOT!<br>`
         document.getElementById("endnote").innerHTML += `* Challenges / Surprise quests<br>`
         document.getElementById("endnote").innerHTML += `* Trophy road rewards<br>`
@@ -311,7 +314,7 @@ function check() {
 }
 
 function cal() {
-    var day = 600 + 30 + 7*5 +10 + 60/4
+    var day = 400 + 200 + 10*3 + 8*5 + 60/4
     var weekend_events = 50 + 500
     if (have_pass) {week_seasonal = 250 * 5 + 500 * 3} 
     else {week_seasonal = 250 * 4 + 500 * 2}
@@ -632,20 +635,22 @@ function solve_brawler(type) {
     document.getElementById("the good shit").innerHTML = ""
     document.getElementById("the good shit").innerHTML += "<div class='content text question' id='tracker'>"
     document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById("tracker").innerHTML += `New Brawler > ${tracke} > Rarity > Questions > Results`
+    document.getElementById("tracker").innerHTML += `${tracke} > ${the_rarity} > Questions > Results`
     make_result_place()
     var the_result = document.getElementById("the result");
     the_result.innerHTML += "Results:<br>"
+    if (the_brawler_name == '')
+        the_brawler_name = "your brawler"
     try
     {
         if(the_sweet_tier[1]=='1')
-            the_result.innerHTML += `You'll get your brawler at tier ${the_sweet_tier[0]} this season.<br>`
+            the_result.innerHTML += `You'll get ${the_brawler_name} at tier ${the_sweet_tier[0]} this season.<br>`
         else
-            the_result.innerHTML += `You'll get your brawler at tier ${the_sweet_tier[0]} next season.<br>`
+            the_result.innerHTML += `You'll get ${the_brawler_name} at tier ${the_sweet_tier[0]} next season.<br>`
     }catch(ReferenceError){}
     
     
-    the_result.innerHTML += `It'll take around ${days_needed} days to get your brawler.<br>`
+    the_result.innerHTML += `It'll take around ${days_needed} days to get ${the_brawler_name}.<br>`
     the_result.innerHTML += `(which is around ${make_date(days_needed)})<br><br>`
 
     if(less_gold < 0)
@@ -658,26 +663,4 @@ function solve_brawler(type) {
     }
     make_end_place(type)
 
-}
-
-function solve_future_days(type) {
-    if(check())
-    {
-        alert("You haven't answered all questions!")
-        return 0
-    }
-    days_asked = document.getElementById("days asked").value
-    var one_day_cre = cal()*25
-    var one_day_end_rewards = cal()
-    document.getElementById("the good shit").innerHTML = ""
-    document.getElementById("the good shit").innerHTML += "<div class='content text question' id='tracker'>"
-    document.getElementById("the good shit").innerHTML += "</div>"
-    document.getElementById("tracker").innerHTML += "Days of Rewards > Fast > Questions > Results"
-    make_result_place()
-    var the_result = document.getElementById("the result");
-    the_result.innerHTML = `In ${days_asked} days, you'll get around ${Math.floor(days_asked*one_day_cre)} credits.`
-    the_result.innerHTML += `<br>`
-    the_result.innerHTML += `Also, you'll get around ${Math.floor(days_asked*one_day_end_rewards*145)} coins and ${Math.floor(days_asked*one_day_end_rewards*42)} power points`
-    the_result.innerHTML += `<br>`
-    the_result.innerHTML += `or ${Math.floor(days_asked*one_day_end_rewards*229)} coins or in between depending on whether your power point bank is full.`
 }
